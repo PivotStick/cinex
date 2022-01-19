9f6295f6c6c9b2d1409b158e0f0f9e4e30b8a229
PivotStick <pivoorigami@gmail.com>
2022-01-19 23:07:33 +0100

    fixed svelte.config.js PROD

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const { PROD, APP_NAME } = process.env;
+const { NODE_ENV, APP_NAME } = process.env;
+
+const PROD = NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
