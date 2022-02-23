03d178e5682e942d1dcce97593b8d0e4b49f63a3
PivotStick <pivoorigami@gmail.com>
2022-02-23 15:20:37 +0100

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 process.env.APP_NAME = 'Cinex';
-
-const { NODE_ENV, APP_NAME } = process.env;
-
-const PROD = NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		ssr: false,
-		paths: {
-			base: PROD ? `/${APP_NAME}`.toLowerCase() : ''
-		},
 		vite: {
 			envPrefix: 'APP_'
 		},
-		adapter: adapter(),
-		target: 'body'
+		adapter: adapter()
 	}
 };
 
